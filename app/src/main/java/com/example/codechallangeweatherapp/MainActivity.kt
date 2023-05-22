d52f549c60b1df57cb53c912b946196a5ba1a0a1
ayman_moubayed <aymanmoubayed@gmail.com>
2023-05-22 12:54:30 -0500

    Take user input and log when submit button is clicked

--- app/src/main/java/com/example/codechallangeweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/codechallangeweatherapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.codechallangeweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textInput: EditText
+    lateinit var city:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun searchCity(view: View) {
+        try{
+            textInput=findViewById(R.id.City)
+            city=textInput.text.toString()
+            println(city)
+
+
+        }  catch (e:Exception){
+            println("exception in api call")
+        }
+
     }
 }
