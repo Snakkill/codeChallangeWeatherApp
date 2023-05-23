b78c087c15ab755bc0e4655709777bec3a571aa0
ayman_moubayed <aymanmoubayed@gmail.com>
2023-05-23 12:00:15 -0500

    +Added location icon +FusedLocation Dependencies +First time asks for permission +Takes cords, uses geocoder to convert then search

--- app/src/main/java/com/example/codechallangeweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/codechallangeweatherapp/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.codechallangeweatherapp
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.codechallenge.Data.WeatherData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 
 const val Base_Url ="https://api.openweathermap.org/data/2.5/"
@@ -22,11 +31,15 @@ const val key="74263b9a49d1177a1f44424e8fad66d9"
 class MainActivity : AppCompatActivity() {
     lateinit var textInput: EditText
     lateinit var city:String
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationProviderClient =LocationServices.getFusedLocationProviderClient(this)
+
     }
+
 
     private fun getWeather(city: String, key: String) {
 
@@ -81,11 +94,38 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-
-
         }  catch (e:Exception){
             println("exception in api call")
         }
 
     }
+
+    fun getlocation(view: View) {
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+            return
+        }
+        task.addOnSuccessListener {
+            if( it !=null){
+                val geoCoder = Geocoder((this), Locale.getDefault())
+                val Adress = geoCoder.getFromLocation(it.latitude,it.longitude,3)
+
+                textInput=findViewById(R.id.City)
+                textInput.setText(Adress[0].locality)
+
+                getWeather(Adress[0].locality, key)
+                println(Adress[0].locality)
+
+                println(it.latitude)
+                println(it.longitude)
+            }
+        }
+    }
+
+
 }
